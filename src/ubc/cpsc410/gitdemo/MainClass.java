65fbf9dc629055dea647bda1937702a67ae4e772
DRADIS <DRADIS@128.189.205.113>
2012-09-11 06:17:19 -0700

    initial commit

---
+++ src/ubc/cpsc410/gitdemo/MainClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package ubc.cpsc410.gitdemo;
+
+/**
+ * @author DRADIS
+ *
+ */
+public class MainClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git is awesome!");
+
+	}
+
+}
